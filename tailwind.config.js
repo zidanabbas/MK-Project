25ae34da869ffcaeaf7fb3f134ccdea4c4c9874f
zidanabbas <zidan.abbas28@gmail.com>
2024-01-31 00:28:30 +0700

    update config tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,28 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
     extend: {
+      screens: {
+        "2xl": "1320px",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        Background: "#0f0e17",
+        Button: "#ff8906",
+        Headline: "#fffffe",
+        Paragraph: "#a7a9be",
+        "Button-text": "#fffffe",
       },
     },
   },
