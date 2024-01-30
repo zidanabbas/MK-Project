93d815ac1713a6b5e770feea603d0988cf99bafa
zidanabbas <zidan.abbas28@gmail.com>
2024-01-31 01:34:14 +0700

    refactor components page.jsx

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,21 +1,10 @@
 import React from "react";
-import Link from "next/link";
+import Header from "@/components/header/header";
 export default function HomePage() {
   return (
-    <main className="min-w-full bg-white-300 overflow-hidden">
-      <nav className="flex w-full items-center px-4 sm:px-24 py-4 justify-between bg-black text-white">
-        <div className="px-2 font-semibold cursor-pointer border">
-          <Link href="/">Nasi Goreng MK</Link>
-        </div>
-        <div className="hidden md:flex">
-          <ul className="flex gap-5 px-2 border">
-            <Link href="/home">Home</Link>
-            <Link href="/menu">Menu</Link>
-            <Link href="/profile">Profile</Link>
-          </ul>
-        </div>
-      </nav>
-      <div className="container flex justify-center bg-red-400 h-screen">
+    <main className="min-w-full min-h-[1000px] bg-Background overflow-hidden">
+      <Header />
+      <div className="container pt-4 flex justify-center mt-12 bg-red-400">
         <div className="container border">
           <h1>Home Page</h1>
         </div>
