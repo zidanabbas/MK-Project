2393cdf9fb33252b8bd98be8faf2cd93aff8284c
zidanabbas <zidan.abbas28@gmail.com>
2024-01-31 23:24:32 +0700

    make a section page

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,12 +1,18 @@
 import React from "react";
-import Header from "@/components/header/header";
+import Header from "@/components/header/Header";
 export default function HomePage() {
   return (
     <main className="min-w-full min-h-[1000px] bg-Background overflow-hidden">
       <Header />
-      <div className="container pt-4 flex justify-center mt-12 bg-red-400">
-        <div className="container border">
-          <h1>Home Page</h1>
+      <div className="w-full min-h-screen flex justify-center pt-16">
+        <div className="container flex gap-2 px-10 items-center justify-between">
+          <div className="container grid grid-cols-2 items-center">
+            <h1>Nasi Goreng MK</h1>
+            <h1>Nasi Goreng MK</h1>
+          </div>
+          <div className="container bg-slate-50">
+            <h1>hello</h1>
+          </div>
         </div>
       </div>
     </main>
