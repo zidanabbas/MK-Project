01038e8b77e3701fd53fa1d621fb22dfc6b2fd5b
zidanabbas <zidan.abbas28@gmail.com>
2024-01-31 23:20:33 +0700

    update colors brand

--- src/components/header/Brand.jsx
+++ src/components/header/Brand.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Brand({ logo }) {
   return (
     <div className="px-4 text-lg font-bold">
-      <Link href="/" className="font-bold py-6 text-lg text-Headline">
+      <Link href="/" className="font-bold py-6 text-lg text-white">
         {logo}
       </Link>
     </div>
