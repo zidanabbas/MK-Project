0e08f6a1db8006790935b968c315e5f381a281f5
zidanabbas <zidan.abbas28@gmail.com>
2024-01-31 23:21:57 +0700

    update color link

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -5,14 +5,14 @@ export default function Navbar() {
   return (
     <>
       <nav className="absolute right-4 top-full hidden lg:flex max-w-[250px] rounded-lg shadow-lg lg:static lg:rounded-none lg:shadow-none lg:bg-transparent">
-        <ul className="flex gap-5 px-2 text-white">
-          <Link href="/home" className="text-Headline">
+        <ul className="flex gap-5 px-2">
+          <Link href="/home" className=" text-white">
             Home
           </Link>
-          <Link href="/menu" className="text-Headline">
+          <Link href="/menu" className="text-white ">
             Menu
           </Link>
-          <Link href="/profile" className="text-Headline">
+          <Link href="/profile" className="text-white">
             Profile
           </Link>
         </ul>
