cea7e215e18d216342d0c8caec6805a2ad451758
zidanabbas <zidan.abbas28@gmail.com>
2024-02-01 21:36:01 +0700

    update components Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,7 +5,7 @@ import MobileNav from "./MobileNav";
 
 export default function Header() {
   return (
-    <header className="fixed py-2 w-full top-0 left-0 z-10 flex items-center bg-Headline">
+    <header className="fixed py-2 w-full -top-1 left-0 z-10 flex items-center bg-Headline">
       <div className="container ">
         <div className="relative flex items-center justify-between">
           <Brand logo={"Nasi Goreng MK"} />
