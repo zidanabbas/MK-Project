3a9dc2956da7351b6e3135049b51cbb3fd94cba9
zidanabbas <zidan.abbas28@gmail.com>
2024-02-01 21:35:17 +0700

    added headers globally to layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header/Header";
 
 const poppins = Poppins({
   weight: ["300", "400", "500", "600", "700"],
@@ -14,7 +15,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
