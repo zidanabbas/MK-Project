6d79c85d215b0cb5e729a05eca9868faa351a7b1
zidanabbas <zidan.abbas28@gmail.com>
2024-02-01 21:34:36 +0700

    create a hero display and section menu

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,20 +1,24 @@
 import React from "react";
-import Header from "@/components/header/Header";
+import Carrousel from "@/components/Carrousel";
+import Hero from "@/components/Hero";
+
 export default function HomePage() {
   return (
     <main className="min-w-full min-h-[1000px] bg-Background overflow-hidden">
-      <Header />
-      <div className="w-full min-h-screen flex justify-center pt-16">
-        <div className="container flex gap-2 px-10 items-center justify-between">
-          <div className="container grid grid-cols-2 items-center">
-            <h1>Nasi Goreng MK</h1>
-            <h1>Nasi Goreng MK</h1>
-          </div>
-          <div className="container bg-slate-50">
-            <h1>hello</h1>
-          </div>
-        </div>
+      <div className="w-full min-h-screen">
+        <Hero />
       </div>
+      <section className="min-h-screen p-0 pt-14">
+        <div className="container">
+          <h1 className="text-2xl px-4 py-2 text-white font-semibold bg-Headline">
+            Our Menu
+          </h1>
+          <h4 className="text-md px-4 py-2 text-Headline font-semibold">
+            Favourite
+          </h4>
+          <Carrousel />
+        </div>
+      </section>
     </main>
   );
 }
