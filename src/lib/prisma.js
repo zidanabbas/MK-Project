00794fa402ea0551b0cca392b6e1ca9050d51175
zidanabbas <zidan.abbas28@gmail.com>
2024-02-04 01:36:51 +0700

    updated prisma.js singleton

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,15 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
-
 let prisma;
 
-if (typeof global !== "undefined" && !global.prisma) {
-  prisma = prismaClientSingleton();
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
 }
 
 export default prisma;
-
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
