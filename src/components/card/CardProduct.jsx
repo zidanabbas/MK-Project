b9cb50a7381955e837d708717d23afd25baef084
zidanabbas <zidan.abbas28@gmail.com>
2024-02-05 22:38:04 +0700

    style: Fixed responsive display of menu cards in CardProduct.jsx

--- src/components/card/CardProduct.jsx
+++ src/components/card/CardProduct.jsx
@@ -3,23 +3,24 @@ import React from "react";
 
 export default function CardProduct() {
   return (
-    <div className="card max-w-96 max-h-sm bg-base-100 shadow-xl">
+    <div className="card max-w-80 max-h-96 md:max-h-[460px] bg-base-100 shadow-xl">
       <figure>
         <Image
           src={
             "https://images.pexels.com/photos/14072794/pexels-photo-14072794.png?auto=compress&cs=tinysrgb&w=600"
           }
           alt="Nasi Goreng Udang"
-          width={800}
-          height={800}
+          className="object-cover h-52 w-full"
+          width={500}
+          height={500}
         />
       </figure>
-      <div className="card-body">
-        <h2 className="card-title">
-          Nasi Goreng Udang
+      <div className="card-body max-h-44 w-full p-4 mt-2 rounded-b-xl">
+        <div className="w-full flex justify-between items-center ">
+          <h2 className="card-title text-md text-white">Nasi Goreng Udang</h2>
           <div className="badge badge-secondary">Popular</div>
-        </h2>
-        <p className="text-base">
+        </div>
+        <p className="text-md text-pretty h-full line-clamp-3 md:line-clamp-none">
           Nasi goreng udang adalah hidangan yang lezat dan menggugah selera,
           menggabungkan cita rasa gurih nasi goreng dengan kelezatan udang yang
           segar.
