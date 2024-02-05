aae0e513db03ac11c19f35b28ec22ae2f6fa1664
zidanabbas <zidan.abbas28@gmail.com>
2024-02-05 23:23:25 +0700

    feat: updated ButtonView

--- src/components/button/page.jsx
+++ src/components/button/page.jsx
@@ -1,15 +1,22 @@
 import Link from "next/link";
 import React from "react";
 
-export default function ButtonView({ href, children }) {
+export default function ButtonView({
+  href,
+  children,
+  type = "button",
+  onClick,
+}) {
   return (
-    <Link
+    <button
       href={href}
       className={
         "text-center mx-auto px-4 py-2 rounded-xl text-Button bg-Headline hover:text-Headline hover:bg-Button duration-300 my-2"
       }
+      type={type}
+      onClick={onClick}
     >
       {children}
-    </Link>
+    </button>
   );
 }
