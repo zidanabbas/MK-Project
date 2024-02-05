1ae5e9ce77a703d773fbe81cedc889b19820282d
zidanabbas <zidan.abbas28@gmail.com>
2024-02-05 22:35:44 +0700

    feat: Add pathname to Navbar.jsx link when switching pages

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,18 +1,32 @@
+"use client";
 import Link from "next/link";
 import React from "react";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const pathname = usePathname();
   return (
     <>
       <nav className="absolute right-4 top-full hidden lg:flex max-w-[250px] rounded-lg shadow-lg lg:static lg:rounded-none lg:shadow-none lg:bg-transparent">
         <ul className="flex gap-5 px-2">
-          <Link href="/home" className=" text-white">
+          <Link
+            href="/"
+            className={`${pathname === "/" ? "text-Button" : "text-white"}`}
+          >
             Home
           </Link>
-          <Link href="/menu" className="text-white ">
+          <Link
+            href="/menu"
+            className={`${pathname === "/menu" ? "text-Button" : "text-white"}`}
+          >
             Menu
           </Link>
-          <Link href="/profile" className="text-white">
+          <Link
+            href="/profile"
+            className={`${
+              pathname === "/profile" ? "text-Button" : "text-white"
+            }`}
+          >
             Profile
           </Link>
         </ul>
