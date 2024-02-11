a7d26609a86768637744be14972f34357a44718e
zidanabbas <zidan.abbas28@gmail.com>
2024-02-11 22:21:10 +0700

    feat: added mobile nav layout.jsx

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header/Header";
+import MobileNav from "@/components/header/MobileNav";
 
 const poppins = Poppins({
   weight: ["300", "400", "500", "600", "700"],
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
       <body className={poppins.className}>
         <Header />
         {children}
+        <MobileNav />
       </body>
     </html>
   );
