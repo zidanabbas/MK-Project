c46a49851fb9a2c4e6f7e5180bc7a6d0c785207f
zidanabbas <zidan.abbas28@gmail.com>
2024-04-01 01:26:35 +0700

    refactor: change the name of the function parameter

--- src/components/header/Brand.jsx
+++ src/components/header/Brand.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Link from "next/link";
 
-export default function Brand({ logo }) {
+export default function Brand({ children }) {
   return (
     <div className="px-4 text-lg font-bold">
       <Link href="/" className="font-bold py-6 text-lg text-white">
-        {logo}
+        {children}
       </Link>
     </div>
   );
