1a6e70d906ce6bf8e66e3b49afb74021d64c4a76
zidanabbas <zidan.abbas28@gmail.com>
2024-04-01 01:23:11 +0700

    feat: added mobile navigation features

--- src/components/header/MobileNav.jsx
+++ src/components/header/MobileNav.jsx
@@ -1,132 +1,45 @@
 import React from "react";
-import Icon from "@mdi/react";
+import Brand from "./Brand";
 import Link from "next/link";
-import {
-  mdiHomeOutline,
-  mdiSilverware,
-  mdiBasketPlusOutline,
-  mdiAccountCircleOutline,
-  mdiCartCheck,
-} from "@mdi/js";
+import Image from "next/image";
+import logoMK from "@/assets/image/logo-brand.png";
 
-export default function MobileNav() {
+export default function NavMobile() {
   return (
-    <>
-      <div className="flex items-center justify-center max-w-full fixed -bottom-10 z-[1000px] sm:hidden">
-        <div className="flex flex-col items-center">
-          <div className="w-full max-w-md px-7 py-10 mx-auto rounded-2xl shadow-xl">
-            <div className="px-16 pt-2 bg-white shadow-lg rounded-3xl">
-              <div className="flex flex-row space-x-3">
-                <div className="flex group">
-                  <Link
-                    href={"/"}
-                    className="p-3 text-gray-400 hover:text-yellow-500"
-                  >
-                    <span className="flex flex-col items-center">
-                      <Icon path={mdiHomeOutline} size={1} title="Home" />
-                      <span className="text-xs mb-2 transition-all duration-200">
-                        Home
-                      </span>
-                      <span
-                        className="h-2 w-2 rounded-full group-hover:bg-yellow-500
-									transition-all duration-150 delay-100"
-                      ></span>
-                    </span>
-                  </Link>
-                </div>
-
-                <div className="flex group">
-                  <Link
-                    href={"/menu"}
-                    className="p-3 text-gray-400 hover:text-yellow-500"
-                  >
-                    <span className="flex flex-col items-center">
-                      <Icon path={mdiSilverware} size={1} title="menu" />
-
-                      <span className="text-xs mb-2 transition-all duration-200">
-                        Menu
-                      </span>
-
-                      <span
-                        className="h-2 w-2 rounded-full group-hover:bg-yellow-500
-									transition-all duration-150 delay-100"
-                      ></span>
-                    </span>
-                  </Link>
-                </div>
-                {/* 
-                <div className="flex group">
-                  <a
-                    href="#"
-                    className="p-3 text-yellow-500 hover:text-yellow-500"
-                  >
-                    <span className="flex flex-col items-center">
-                      <Icon
-                        path={mdiBasketPlusOutline}
-                        size={1}
-                        title="Feeds"
-                      />
-
-                      <span className="text-xs mb-2 transition-all duration-200">
-                        Feeds
-                      </span>
-
-                      <span
-                        className="h-1 w-5 rounded-full bg-yellow-500 group-hover:bg-yellow-500
-									hover:h-3 hover:w-3 transition-all duration-150 delay-100"
-                      ></span>
-                    </span>
-                  </a>
-                </div> */}
-
-                <div className="flex group">
-                  <Link
-                    href={"/cart"}
-                    className="p-3 text-gray-400 hover:text-yellow-500"
-                  >
-                    <span className="flex flex-col items-center">
-                      <Icon path={mdiCartCheck} size={1} title="Cart" />
-
-                      <span className="text-xs mb-2 transition-all duration-200">
-                        Cart
-                      </span>
-
-                      <span
-                        className="h-2 w-2 rounded-full group-hover:bg-yellow-500
-									transition-all duration-150 delay-100"
-                      ></span>
-                    </span>
-                  </Link>
-                </div>
-
-                <div className="flex group">
-                  <a
-                    href="#"
-                    className="p-3 text-gray-400 hover:text-yellow-500"
-                  >
-                    <span className="flex flex-col items-center">
-                      <Icon
-                        path={mdiAccountCircleOutline}
-                        size={1}
-                        title="Account"
-                      />
-
-                      <span className="text-xs mb-2 transition-all duration-200">
-                        Account
-                      </span>
-
-                      <span
-                        className="h-2 w-2 rounded-full group-hover:bg-yellow-500
-									transition-all duration-150 delay-100"
-                      ></span>
-                    </span>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+    <nav className="lg:hidden absolute top-12 right-9 bg-Headline py-5 shadow-2xl rounded-lg max-w-64 w-full">
+      <div className="w-20">
+        <Brand>
+          <Image src={logoMK} alt="Logo MK" width={40} height={50} />
+        </Brand>
       </div>
-    </>
+      <nav className="flex items-center h-full justify-center">
+        <ul className="block">
+          <div className="group">
+            <Link
+              href="/"
+              className="text-base text-white py-2 mx-8 flex group-hover:text-Button"
+            >
+              Home
+            </Link>
+          </div>
+          <div className="group">
+            <Link
+              href="/menu"
+              className="text-base text-white py-2 mx-8 flex group-hover:text-Button"
+            >
+              Menu
+            </Link>
+          </div>
+          <div className="group">
+            <Link
+              href="/location"
+              className="text-base text-white py-2 mx-8 flex group-hover:text-Button"
+            >
+              Location
+            </Link>
+          </div>
+        </ul>
+      </nav>
+    </nav>
   );
 }
