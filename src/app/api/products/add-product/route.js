9524671893f6657f337d6db310c62e039e61271e
zidanabbas <zidan.abbas28@gmail.com>
2024-04-01 02:06:53 +0700

    feat: added hero section

--- src/app/api/products/add-product/route.js
+++
@@ -1,7 +0,0 @@
-import prisma from "@/lib/prisma";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const product = await prisma.products.findMany();
-  return NextResponse.json(product);
-}
