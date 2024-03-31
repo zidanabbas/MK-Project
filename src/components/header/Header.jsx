6f29b674bca67783ea9a2df586156304d963cb18
zidanabbas <zidan.abbas28@gmail.com>
2024-04-01 01:22:10 +0700

    fix: Fixed menu bar header for mobile

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,38 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Navbar";
 import Brand from "./Brand";
+import Image from "next/image";
+import logoMK from "@/assets/image/logo-brand.png";
+import MobileNav from "./MobileNav";
+import Icon from "@mdi/react";
+import { mdiMenu, mdiMenuOpen } from "@mdi/js";
 
 export default function Header() {
+  const [isCLicked, setIsClicked] = useState(false);
+  const toggleMenu = () => {
+    setIsClicked(!isCLicked);
+  };
   return (
-    <header className="fixed py-2 w-full -top-1 left-0 z-10 flex items-center bg-Headline">
-      <div className="container ">
+    <header className="fixed h-12 py-2 w-full -top-1 left-0 z-10 flex items-center bg-Headline">
+      <div className="container relative">
         <div className="relative flex items-center justify-between">
-          <Brand logo={"Nasi Goreng MK"} />
-          <div className="flex items-center px-4">
+          <Brand>
+            <Image src={logoMK} alt="Logo MK" width={40} height={50} />
+          </Brand>
+          <div className="flex items-center px-4 gap-2">
             <Navbar />
+            <button className="lg:hidden" onClick={toggleMenu}>
+              {isCLicked ? (
+                <Icon path={mdiMenuOpen} size={2} className="p-1 text-white" />
+              ) : (
+                <Icon path={mdiMenu} size={2} className="p-1 text-white" />
+              )}
+            </button>
           </div>
         </div>
       </div>
+      {isCLicked && <MobileNav />}
     </header>
   );
 }
