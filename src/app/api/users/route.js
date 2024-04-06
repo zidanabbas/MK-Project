705b3521cb09c46b8212b0ec710dec92cb84dfa5
zidanabbas <zidan.abbas28@gmail.com>
2024-04-06 23:20:21 +0700

    refactor: clean unused code and files

--- src/app/api/users/route.js
+++
@@ -1,7 +0,0 @@
-// import prisma from "@/lib/prisma";
-// import { NextResponse } from "next/server";
-
-// export async function GET() {
-//   const user = await prisma.user.findMany();
-//   return NextResponse.json(user);
-// }
