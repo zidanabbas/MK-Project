c7c6298d5c7f415d8b8696b1a34d70c6daf6b148
zidanabbas <zidan.abbas28@gmail.com>
2024-04-06 23:17:35 +0700

    feat: create dynamic menu cards drawn from api

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,16 +1,31 @@
-import React from "react";
-import CardMenuBody from "./CardMenuBody";
+"use client";
+import React, { useEffect, useState } from "react";
+import CardMenu from "../fragments/CardMenu";
+import getProducts from "@/services/product-service";
 
-export default function CardMenu({ titleMenuName, alt, src, name }) {
+export default function Menu() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getProducts((data) => {
+      setProducts(data);
+    });
+  }, []);
+
   return (
     <div className="bg-white">
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
-        <div className="text-center mb-12">
-          <h2 className="text-3xl leading-9 font-extrabold text-gray-900">
-            {titleMenuName}
-          </h2>
+        <div className="p-2 grid grid-cols-1 md:grid-cols-4 gap-4 w-full">
+          {products.map((product) => (
+            <CardMenu key={product.id}>
+              <CardMenu.Header alt={product.title} src={product.image} />
+              <CardMenu.Body title={product.title}>
+                {product.description}
+              </CardMenu.Body>
+              <CardMenu.Footer price={product.price} />
+            </CardMenu>
+          ))}
         </div>
-        <CardMenuBody alt={alt} src={src} name={name} />
       </div>
     </div>
   );
