e70d5436703803fc410e27cf6ddf25252413bfe6
zidanabbas <zidan.abbas28@gmail.com>
2024-04-06 23:18:21 +0700

    style: change height screen layout

--- src/components/layouts/section/page.jsx
+++ src/components/layouts/section/page.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function ContainerSection({ children }) {
   return (
-    <section className="min-h-screen pt-14 pb-16 border border-b-black">
+    <section className="min-h-[500px] pt-14 pb-16 border border-b-black">
       {children}
     </section>
   );
