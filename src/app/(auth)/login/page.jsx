e414f86763b8a2b1e9567d554c25dfc539bd55a7
zidanabbas <zidan.abbas28@gmail.com>
2024-05-01 23:58:38 +0700

    feat: create authentication login and registration pages

---
+++ src/app/(auth)/login/page.jsx
@@ -0,0 +1,14 @@
+import FormLogin from "@/components/fragments/FormLogin";
+import AuthLayouts from "@/components/layouts/AuthLayouts/page";
+import ContainerBody from "@/components/layouts/body/page";
+import React from "react";
+
+export default function LoginPage() {
+  return (
+    <ContainerBody classname={"min-h-screen grid items-center"}>
+      <AuthLayouts title={"Login"} desc={"Login your account."}>
+        <FormLogin />
+      </AuthLayouts>
+    </ContainerBody>
+  );
+}
---
+++ src/app/(auth)/register/page.jsx
@@ -0,0 +1,14 @@
+import FormRegister from "@/components/fragments/FormRegister";
+import AuthLayouts from "@/components/layouts/AuthLayouts/page";
+import ContainerBody from "@/components/layouts/body/page";
+import React from "react";
+
+export default function RegisterPage() {
+  return (
+    <ContainerBody classname={"min-h-screen grid items-center"}>
+      <AuthLayouts title={"Register"} desc={"Register for you'r account."}>
+        <FormRegister />
+      </AuthLayouts>
+    </ContainerBody>
+  );
+}
