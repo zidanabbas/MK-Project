64e7cfd98c54a04cb385071497948c358a34d7c0
zidanabbas <zidan.abbas28@gmail.com>
2024-05-14 22:33:09 +0700

    feat : make API call, get Product Details

--- src/services/product-service.js
+++ src/services/product-service.js
@@ -1,11 +1,27 @@
-const getProducts = async (callback) => {
+export const getProducts = async (callback) => {
   try {
-    const response = await fetch(process.env.NEXT_PUBLIC_API_PRODUCTS);
-    const data = await response.json();
+    const response = await fetch(process.env.NEXT_PUBLIC_PRODUCTS, {
+      cache: "no-store",
+    });
+    const { data } = await response.json();
     callback(data);
   } catch (error) {
-    console.log(error);
+    console.error(error);
   }
 };
 
-export default getProducts;
+export const getDetailProduct = async (id, callback) => {
+  try {
+    const response = await fetch(process.env.NEXT_PUBLIC_PRODUCTS + id);
+    if (!response.ok) {
+      throw new Error("Failed to fetch data");
+    }
+    const responseData = await response.json();
+    if (!responseData.data) {
+      throw new Error("Product data not found");
+    }
+    callback(responseData.data);
+  } catch (error) {
+    console.error(error);
+  }
+};
