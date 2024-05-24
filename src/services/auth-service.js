09e33cae7f35e27658b2db79120222446e2606f6
zidanabbas <zidan.abbas28@gmail.com>
2024-05-24 23:31:35 +0700

    feat : make auth-service api call in services folder

---
+++ src/services/auth-service.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { jwtDecode } from "jwt-decode";
+
+const authEndPoint = process.env.NEXT_PUBLIC_AUTH;
+
+export const login = (formData, callback) => {
+  axios
+    .post(`${authEndPoint}/login`, formData)
+    .then((res) => {
+      callback(true, res.data);
+    })
+    .catch((err) => {
+      callback(false, err);
+    });
+};
+
+export const register = (data, callback) => {
+  axios
+    .post(`${authEndPoint}/register`, data)
+    .then((res) => {
+      callback(true, res.data);
+    })
+    .catch((err) => {
+      callback(false, err);
+    });
+};
+
+export const getUsername = (token) => {
+  const decoded = jwtDecode(token);
+  console.log(decoded);
+};
