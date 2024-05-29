bd05909a228f9f432ce4c58c0b66629bfa1e87ad
zidanabbas <zidan.abbas28@gmail.com>
2024-05-29 23:18:30 +0700

    style : make a responsif layouts and fixing path components

--- src/app/menu/page.jsx
+++ src/app/menu/page.jsx
@@ -5,7 +5,9 @@ import { getProducts } from "@/services/product-service";
 import { useState, useEffect } from "react";
 import Link from "next/link";
 import CardMenu from "@/components/fragments/CardMenu";
-import BackButton from "@/components/fragments/BackButton";
+import BackButton from "@/components/ui/BackButton";
+import Button from "@/components/ui/Button";
+import Layouts from "@/components/layouts/BodyLayouts/page";
 
 export default function PageMenu() {
   const [products, setProducts] = useState([]);
@@ -36,18 +38,18 @@ export default function PageMenu() {
   }, [search, products]);
 
   return (
-    <>
-      <main className="container mx-auto px-4 md:px-6 py-12 md:py-16 lg:py-20">
-        <div className="mb-8 md:mb-12 lg:mb-16 bg-Headline rounded-lg px-7 py-4">
-          <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold tracking-tight text-white">
+    <Layouts>
+      <main className="container mx-auto px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 py-12 md:py-16 lg:py-20 xl:py-24">
+        <div className="mb-8 md:mb-12 lg:mb-16 xl:mb-20 bg-Headline rounded-lg px-7 py-4">
+          <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold tracking-tight text-white">
             Our Delicious Menu
           </h1>
-          <div className="mt-2 md:mt-3 lg:mt-4 text-white">
+          <div className="mt-2 sm:mt-3 md:mt-4 lg:mt-5 xl:mt-6 text-white">
             Explore our wide selection of mouthwatering dishes.
           </div>
         </div>
         <BackButton href={"/"} />
-        <div className="flex items-center mb-6 md:mb-8 lg:mb-10 max-w-80">
+        <div className="flex items-center mb-6 sm:mb-8 md:mb-10 lg:mb-12 xl:mb-14 max-w-80">
           <div className="flex flex-col gap-2 w-full">
             <h4 className="text-gray-700 font-poppins">Search Products :</h4>
             <input
@@ -58,7 +60,7 @@ export default function PageMenu() {
             />
           </div>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-4 place-items-center md:gap-10 items-center px-4 py-2">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 place-items-center gap-4 sm:gap-6 md:gap-8 lg:gap-10 xl:gap-12 px-4 py-2">
           {searchResult.map((product) => (
             <Link href={`/menu/${product.id}`} key={product.id}>
               <CardMenu key={product.id}>
@@ -66,12 +68,14 @@ export default function PageMenu() {
                 <CardMenu.Body title={product.title}>
                   {product.description}
                 </CardMenu.Body>
-                <CardMenu.Footer price={product.price} />
+                <CardMenu.Footer price={product.price}>
+                  <Button>Detail</Button>
+                </CardMenu.Footer>
               </CardMenu>
             </Link>
           ))}
         </div>
       </main>
-    </>
+    </Layouts>
   );
 }
