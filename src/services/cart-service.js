146de9964450a516ac1b6f08b01e91c3f552b9a9
zidanabbas <zidan.abbas28@gmail.com>
2024-05-30 23:09:29 +0700

    feat : Create a carts api call service

---
+++ src/services/cart-service.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const authEndPoint = process.env.NEXT_PUBLIC_URL;
+
+export const getCart = async (callback) => {
+  try {
+    const response = await axios.get(`${authEndPoint}/carts`);
+    callback(response.data);
+  } catch (error) {
+    callback(error);
+  }
+};
+export const addToCart = async (formData, callback) => {
+  try {
+    const response = await axios.post(`${authEndPoint}/carts`, formData);
+    callback(response.data);
+  } catch (error) {
+    callback(error);
+  }
+};
