c15a943b0215a1f14a7f59ce9c858602cc6ad402
zidanabbas <zidan.abbas28@gmail.com>
2024-06-03 17:52:35 +0700

    feat : create a button cart

---
+++ src/components/ui/Button/CartButton.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { useSelector } from "react-redux";
+import ModalCart from "../fragments/ModalCart";
+
+export default function CartIcon() {
+  const cartItems = useSelector((state) => state.cart.data);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleCart = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <button
+      className="flex items-center justify-center p-2 relative w-14 text-center"
+      onClick={toggleCart}
+    >
+      <span className="absolute top-0 right-2 rounded-full bg-red-700 w-5 mt-1">
+        {cartItems?.length ?? 0}
+      </span>
+      <AiOutlineShoppingCart size={25} />
+      {isOpen && <ModalCart />}
+    </button>
+  );
+}
