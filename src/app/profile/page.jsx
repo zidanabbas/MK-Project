f23a3fed52ba1d46fa1da56dff2f207598e7c1f9
zidanabbas <zidan.abbas28@gmail.com>
2024-06-03 23:56:25 +0700

    update : delete wrap fragments

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,3 +1,4 @@
+"use client";
 import Hero from "@/components/ui/Hero";
 import ProfilOwner from "@/components/fragments/ProfilOwner";
 import Layouts from "@/components/layouts/BodyLayouts/page";
@@ -6,26 +7,24 @@ import SectionPage from "@/components/layouts/SectionPage/SectionPage";
 
 export default function ProfilePage() {
   return (
-    <>
-      <Layouts>
-        <Hero
-          title={"PROFESSIONAL CHEF"}
-          description={"The man behind the kitchen"}
-          style={{
-            backgroundImage:
-              "url(https://images.unsplash.com/photo-1588416820614-f8d6ac6cea56?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDh8fGtpdGNoZW4lMjByZXN0YXVyYW50fGVufDB8fDB8fHww)",
-          }}
+    <Layouts>
+      <Hero
+        title={"PROFESSIONAL CHEF"}
+        description={"The man behind the kitchen"}
+        style={{
+          backgroundImage:
+            "url(https://images.unsplash.com/photo-1588416820614-f8d6ac6cea56?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDh8fGtpdGNoZW4lMjByZXN0YXVyYW50fGVufDB8fDB8fHww)",
+        }}
+      />
+      <SectionPage title={"Profil"} description={"Profile Owner"}>
+        <ProfilOwner
+          alt={"Muhammad Kadir Panjaitan"}
+          nameOwner={"Muhammad Kadir Panjaitan"}
+          description={
+            "Chef Muhammad Kadir Panjaitan telah berkontribusi secara signifikan dalam membawa pengalaman kuliner yang luar biasa di Restoran SAKURA. Dengan dedikasi terhadap detail dan rasa, Chef Muhammad Kadir Panjaitan terus berusaha untuk memberikan yang terbaik di setiap hidangan yang disajikan."
+          }
         />
-        <SectionPage title={"Profil"} description={"Profile Owner"}>
-          <ProfilOwner
-            alt={"Muhammad Kadir Panjaitan"}
-            nameOwner={"Muhammad Kadir Panjaitan"}
-            description={
-              "Chef Muhammad Kadir Panjaitan telah berkontribusi secara signifikan dalam membawa pengalaman kuliner yang luar biasa di Restoran SAKURA. Dengan dedikasi terhadap detail dan rasa, Chef Muhammad Kadir Panjaitan terus berusaha untuk memberikan yang terbaik di setiap hidangan yang disajikan."
-            }
-          />
-        </SectionPage>
-      </Layouts>
-    </>
+      </SectionPage>
+    </Layouts>
   );
 }
