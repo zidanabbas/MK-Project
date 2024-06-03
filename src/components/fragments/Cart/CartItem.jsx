4852f7b6be246d9eb4d15f3c206326cbc8e97693
zidanabbas <zidan.abbas28@gmail.com>
2024-06-03 23:58:44 +0700

    refactor : updated CartItem and added a close icon

--- src/components/fragments/Cart/CartItem.jsx
+++ src/components/fragments/Cart/CartItem.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import CartItemBody from "@/components/fragments/CartItemBody/CartItemBody";
 import Button from "@/components/ui/Button/Button";
+import { IoIosCloseCircleOutline } from "react-icons/io";
 
 export default function CartItem({
   src,
@@ -10,18 +11,17 @@ export default function CartItem({
   quantity,
   onClick,
   id,
-  totalPrice,
 }) {
   return (
-    <div className="flex items-center mt-4  justify-evenly ">
+    <div className="flex items-center  justify-evenly ">
       <div className="w-full max-w-2xl p-4 bg-white rounded-lg shadow-md my-3 relative">
         <Button
           classname={
-            "absolute bg-transparent flex justify-center top-0 w-6 h-6 right-0 items-center"
+            "absolute bg-transparent flex justify-center top-0 right-0 items-center"
           }
           onClick={onClick}
         >
-          x
+          <IoIosCloseCircleOutline size={20} />
         </Button>
         <ul className="divide-y divide-gray-200">
           <CartItemBody
@@ -33,12 +33,6 @@ export default function CartItem({
             id={id}
           />
         </ul>
-      </div>
-      <div className="mt-4 flex justify-between items-center">
-        <span className="text-lg font-bold">{totalPrice}</span>
-        <button className="px-6 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 focus:outline-none">
-          Checkout
-        </button>
       </div>
     </div>
   );
