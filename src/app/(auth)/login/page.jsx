a18e9408817d574c5f59fb80126cbde4904541ea
zidanabbas <zidan.abbas28@gmail.com>
2024-06-04 00:09:47 +0700

    updated code

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,14 +1,15 @@
+"use client";
 import FormLogin from "@/components/fragments/FormLogin";
 import AuthLayouts from "@/components/layouts/AuthLayouts/page";
-import ContainerBody from "@/components/layouts/BodyLayouts/page";
+import Layouts from "@/components/layouts/BodyLayouts/page";
 import React from "react";
 
 export default function LoginPage() {
   return (
-    <ContainerBody classname={"min-h-screen grid items-center"}>
+    <Layouts classname={"min-h-screen grid items-center"}>
       <AuthLayouts title={"Login"} desc={"Login your account."}>
         <FormLogin />
       </AuthLayouts>
-    </ContainerBody>
+    </Layouts>
   );
 }
--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,14 +1,15 @@
+"use client";
 import FormRegister from "@/components/fragments/FormRegister";
 import AuthLayouts from "@/components/layouts/AuthLayouts/page";
-import ContainerBody from "@/components/layouts/BodyLayouts/page";
+import Layouts from "@/components/layouts/BodyLayouts/page";
 import React from "react";
 
 export default function RegisterPage() {
   return (
-    <ContainerBody classname={"min-h-screen grid items-center"}>
+    <Layouts classname={"min-h-screen grid items-center"}>
       <AuthLayouts title={"Register"} desc={"Register for you'r account."}>
         <FormRegister />
       </AuthLayouts>
-    </ContainerBody>
+    </Layouts>
   );
 }
