b2826f9d7245d7a6209bc2a274db80eab33c6358
zidanabbas <zidan.abbas28@gmail.com>
2024-06-04 00:02:55 +0700

    refactor : updated CartButton

--- src/components/ui/Button/CartButton.jsx
+++ src/components/ui/Button/CartButton.jsx
@@ -1,26 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { useSelector } from "react-redux";
-import ModalCart from "../../fragments/ModalCart";
 
-export default function CartButton() {
+export default function CartButton({ onClick }) {
   const cartItems = useSelector((state) => state.cart.data);
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleCart = () => {
-    setIsOpen(!isOpen);
-  };
 
   return (
     <button
       className="flex items-center justify-center p-2 relative w-14 text-center"
-      onClick={toggleCart}
+      onClick={onClick}
     >
       <span className="absolute top-0 right-2 rounded-full bg-red-700 w-5 mt-1">
         {cartItems?.length ?? 0}
       </span>
       <AiOutlineShoppingCart size={25} />
-      {isOpen && <ModalCart />}
     </button>
   );
 }
