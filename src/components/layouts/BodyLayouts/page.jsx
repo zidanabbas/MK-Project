a8e089a3a18b46502c7ec8199d4125f82b8bb357
zidanabbas <zidan.abbas28@gmail.com>
2024-06-04 00:02:05 +0700

    feat : added animated transitions from framer motion

--- src/components/layouts/BodyLayouts/page.jsx
+++ src/components/layouts/BodyLayouts/page.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 export default function Layouts({ children, classname }) {
   return (
     <>
-      <main className={`w-full min-h-screen ${classname} overflow-hidden`}>
+      <motion.div
+        animate={{ opacity: 1, scale: 1 }}
+        initial={{ opacity: 0 }}
+        transition={{
+          duration: 1,
+          delay: 0.8,
+          ease: "easeIn",
+        }}
+        className={`w-full min-h-screen ${classname} overflow-hidden`}
+      >
         {children}
-      </main>
+      </motion.div>
     </>
   );
 }
