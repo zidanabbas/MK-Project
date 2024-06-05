e7951ee515b199cf8ddea81ebfa6c0046f770822
zidanabbas <zidan.abbas28@gmail.com>
2024-06-05 22:46:22 +0700

    fixing env

--- next.config.mjs
+++ next.config.mjs
@@ -27,7 +27,7 @@ const nextConfig = {
   },
   env: {
     NEXT_PUBLIC_BASE_URL:
-      process.env.NEXT_PUBLIC_BASE_URL === "production"
+      process.env.NODE_ENV === "production"
         ? "https://nasigorengmk.vercel.app/"
         : "http://localhost:3000/",
   },
